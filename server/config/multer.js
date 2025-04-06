bd08ecd8846fde2b1712409c877d854313b27404
metto <metto@MacBook-Pro-metto.local>
2025-04-06 19:35:21 +0300

    deleted comments

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,10 +1,9 @@
 const multer = require("multer");
 const path = require("path");
 
-// Configure storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images/products"); // Make sure this directory exists
+    cb(null, "public/images/products");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -13,7 +12,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// File filter to allow only images
 const fileFilter = (req, file, cb) => {
   const filetypes = /jpeg|jpg|png|gif/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
@@ -28,7 +26,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: fileFilter,
 });
 
